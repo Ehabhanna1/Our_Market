f417170af745f28bd7fc84c0889e15d2715f8af0
Ehabhanna1 <ehabhanna1599@gmail.com>
2025-02-07 22:41:10 +0200

    start profile view ui

--- lib/feature/profile/ui/profile_view.dart
+++ lib/feature/profile/ui/profile_view.dart
@@ -1,4 +1,8 @@
+import 'package:ecommerce_app/core/helper/spacing.dart';
+import 'package:ecommerce_app/core/theming/app_colors.dart';
+import 'package:ecommerce_app/core/theming/styles.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class ProfileView extends StatelessWidget {
   const ProfileView({super.key});
@@ -6,7 +10,62 @@ class ProfileView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(child: Text("Profile View"),)
+      body: SafeArea(
+        child:Card(
+          color: AppColors.kWhiteColor,
+          child: Padding(
+            padding:EdgeInsets.all(16.0),
+            child: Column(
+              children: [
+                CircleAvatar(
+                  radius: 60.r,
+                  backgroundColor: AppColors.kPrimaryColor,
+                  foregroundColor: AppColors.kWhiteColor,
+                  child: Icon(
+                    Icons.person,
+                    size: 45.r,
+                  ),
+                  
+                ),
+                verticalSpace(10),
+                Text(
+                  "user name",
+                  style: TextStyles.font18BlackBold,
+                ),
+                verticalSpace(10),
+                Text(
+                  "user email",
+                  style: TextStyles.font15DarkBlueMedium,
+                ),
+                verticalSpace(15),
+                Card(
+                  color: AppColors.kPrimaryColor,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10.r),
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Icon(Icons.person_2_outlined, color: AppColors.kWhiteColor,),
+                    
+                        Text("Edit Profile", style: TextStyles.font18WhiteMedium),
+                        Icon(Icons.arrow_forward_ios, color: AppColors.kWhiteColor,)
+                    
+                    
+                    
+                    ],),
+                  ),
+                ),
+               
+              ],
+            ),
+            
+            ),
+          
+          
+          ),),
     );
   }
 }
