673528b57fffaddfc9df12d1d648c77d60cb5a0a
Ehabhanna1 <ehabhanna1599@gmail.com>
2025-02-07 22:21:41 +0200

    finish favorite view ui

--- lib/feature/favorites/ui/favorites_view.dart
+++ lib/feature/favorites/ui/favorites_view.dart
@@ -1,11 +1,29 @@
+import 'package:ecommerce_app/core/helper/spacing.dart';
+import 'package:ecommerce_app/core/theming/styles.dart';
+import 'package:ecommerce_app/core/widgets/products_list_view.dart';
 import 'package:flutter/material.dart';
 class FavoritesView extends StatelessWidget {
   const FavoritesView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(child: Text("Favorites View"),)
+    return Padding(
+      
+      padding: const EdgeInsets.all(10),
+      child: ListView(
+        children: [
+          Text("Your Favorite Products",
+          textAlign: TextAlign.center,
+          style: TextStyles.font24BlackBold,
+          ),
+          verticalSpace( 10),
+         
+           ProductsListView(),
+      ],
+      
+      
+         
+      ),
     );
   }
 }
