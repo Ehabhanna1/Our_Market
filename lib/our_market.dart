5edb3b97378db5ce127197089539ea0a192f276b
Ehabhanna1 <ehabhanna1599@gmail.com>
2025-02-08 18:19:26 +0200

    check if current user exist to the app

--- lib/our_market.dart
+++ lib/our_market.dart
@@ -1,24 +1,29 @@
 import 'package:ecommerce_app/feature/auth/logic/cubit/authentication_cubit.dart';
 import 'package:ecommerce_app/feature/auth/ui/login_view.dart';
+import 'package:ecommerce_app/feature/main_nav_bar/ui/main_nav_bar.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
 class OurMarket extends StatelessWidget {
   const OurMarket({super.key});
 
   @override
   Widget build(BuildContext context) {
+
+    SupabaseClient client = Supabase.instance.client;
+     
     return ScreenUtilInit(
       designSize: const Size(375, 812),
       minTextAdapt: true,
       splitScreenMode: true,
       builder: (context, child) => BlocProvider(
         create: (context) => AuthenticationCubit(),
-        child: const MaterialApp(
+        child: MaterialApp(
           debugShowCheckedModeBanner: false,
           title: 'Our Market',
-          home: LoginView(),
+          home: client.auth.currentUser != null? MainNavBar() : LoginView(),
         ),
       ),
     );
